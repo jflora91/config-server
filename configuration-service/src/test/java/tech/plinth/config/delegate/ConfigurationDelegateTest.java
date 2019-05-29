e09401c8078d02826b5f4b877e77a7672f5473c2
João Flora <joao.flora@mindera.com>
2019-05-29 16:42:38 +0100

    add more tests

--- configuration-service/src/test/java/tech/plinth/config/delegate/ConfigurationDelegateTest.java
+++ configuration-service/src/test/java/tech/plinth/config/delegate/ConfigurationDelegateTest.java
@@ -12,11 +12,17 @@ import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import tech.plinth.config.database.model.Base;
+import tech.plinth.config.database.model.Configuration;
 import tech.plinth.config.database.repository.BaseRepository;
 import tech.plinth.config.database.repository.ConfigurationRepository;
 import tech.plinth.config.interceptor.model.RequestContext;
 
+import java.io.IOException;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
 @EnableAutoConfiguration(exclude = FlywayAutoConfiguration.class)
@@ -56,13 +62,53 @@ public class ConfigurationDelegateTest {
     }
 
     @Test
-    public void getScopeTest() throws JsonPatchException {
-        String scope = "config1";
+    public void getBlankScopeTest() throws JsonPatchException {
         try {
             configurationDelegate.getScope("");
         } catch (ResponseStatusException ex) {
             assertEquals(ex.getStatus(), HttpStatus.BAD_REQUEST);
         }
+        try {
+            configurationDelegate.getScope(null);
+        } catch (ResponseStatusException ex) {
+            assertEquals(ex.getStatus(), HttpStatus.BAD_REQUEST);
+        }
+        try {
+            configurationDelegate.getScope(" ");
+        } catch (ResponseStatusException ex) {
+            assertEquals(ex.getStatus(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Test
+    public void getScopeTest() throws JsonPatchException, IOException {
+        String scope;
+
+        baseJsonNode = mapper.readTree("{" +
+                "            \"config1\": \"config1\"," +
+                "            \"config3\": {" +
+                "                \"config31\": \"config3.1\"," +
+                "                \"config32\": \"config3.2\"" +
+                "            }" +
+                "        }");
+
+        configurationJsonNode = mapper.readTree("{" +
+                "            \"config1\": {" +
+                "                \"config11\": \"config1.1\"," +
+                "                \"config12\": null" +
+                "            }" +
+                "        }");
+
+        Base base = new Base(baseVersion, baseJsonNode);
+        Configuration configuration = new Configuration(configurationPlatform, configurationJsonNode, configurationVersion);
+
+        when(configurationRepository.findTopByPlatformOrderByVersionDesc(configurationPlatform)).thenReturn(Optional.of(configuration));
+        when(baseRepository.findTopByOrderByVersionDesc()).thenReturn(Optional.of(base));
+        scope = "config2";
+        assertNull(configurationDelegate.getScope(scope));
+        scope = "config1";
+        assertEquals(configurationDelegate.getScope(scope), mapper.readTree("{\"config11\": \"config1.1\"}"));
+
     }
 }
 
