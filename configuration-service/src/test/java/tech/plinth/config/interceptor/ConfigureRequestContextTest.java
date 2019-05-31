897093460d2f6fa4dfec93f03020c557c194ccc9
João Flora <joao.flora@mindera.com>
2019-05-31 15:52:46 +0100

    just for save

--- configuration-service/src/test/java/tech/plinth/config/interceptor/ConfigureRequestContextTest.java
+++ configuration-service/src/test/java/tech/plinth/config/interceptor/ConfigureRequestContextTest.java
@@ -1,7 +1,5 @@
 package tech.plinth.config.interceptor;
 
-import org.apache.struts.mock.MockHttpServletRequest;
-import org.apache.struts.mock.MockHttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,30 +8,28 @@ import org.mockito.Mock;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tech.plinth.config.interceptor.model.RequestContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static tech.plinth.config.interceptor.ConfigureRequestContext.HEADER_PLATFORM_ID;
 
 @ExtendWith(SpringExtension.class)
 @EnableAutoConfiguration(exclude = FlywayAutoConfiguration.class)
 @SpringBootTest
 public class ConfigureRequestContextTest {
 
-    private MockHttpServletRequest request = new MockHttpServletRequest();
-    private MockHttpServletResponse response = new MockHttpServletResponse();
-
     @Mock
     RequestContext requestContext;
-
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+    private MockHttpServletResponse response = new MockHttpServletResponse();
     @InjectMocks
     private ConfigureRequestContext configureRequestContext;
 
     @BeforeEach
     public void setUp() {
-//        when(request.getHeader(HEADER_PLATFORM_ID)).thenReturn(null);
+//         when(request.getHeader(HEADER_PLATFORM_ID)).thenReturn(null);
     }
 
     @Test
