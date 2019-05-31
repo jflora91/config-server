5da665e6ec785d769029c7caeb415b5ae82f5e07
João Flora <joao.flora@mindera.com>
2019-05-31 09:59:16 +0100

    ongoing

--- configuration-service/src/test/java/tech/plinth/config/interceptor/ConfigureRequestContextTest.java
+++ configuration-service/src/test/java/tech/plinth/config/interceptor/ConfigureRequestContextTest.java
@@ -1,15 +1,44 @@
 package tech.plinth.config.interceptor;
 
+import org.apache.struts.mock.MockHttpServletRequest;
+import org.apache.struts.mock.MockHttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tech.plinth.config.interceptor.model.RequestContext;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static tech.plinth.config.interceptor.ConfigureRequestContext.HEADER_PLATFORM_ID;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration(exclude = FlywayAutoConfiguration.class)
+@SpringBootTest
 public class ConfigureRequestContextTest {
+
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+    private MockHttpServletResponse response = new MockHttpServletResponse();
+
+    @Mock
+    RequestContext requestContext;
+
+    @InjectMocks
+    private ConfigureRequestContext configureRequestContext;
+
+    @BeforeEach
+    public void setUp() {
+//        when(request.getHeader(HEADER_PLATFORM_ID)).thenReturn(null);
+    }
 
     @Test
     public void preHandleTest() {
 
+        assertEquals(requestContext.getPlatformId(), null);
     }
 }
